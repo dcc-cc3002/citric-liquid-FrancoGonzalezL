639a8b41afa03acd068f22914bcee1b229f017d6
FrancoGonzalezL <franco.gonzalez.l@ug.uchile.cl>
2023-12-05 21:36:38 -0300

    feat(Main): added main to play the Game.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,8 +7,8 @@ import scala.util.Random
 object Main {
     def main(args: Array[String]): Unit = {
         val game: GameController = new GameController
-        var x: Map[Int, Int] = Map()
 
-
+        game.startGame()
+        game.run()
     }
 }
